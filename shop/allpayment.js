f460078eb1aaacb77c8c93936c32a6ade13f1c90
YOGI ANDARU <83377619+yogiandaru98@users.noreply.github.com>
2021-11-17 23:37:22 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,21 +1,16 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
-
-*さ BANK*
-1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
-		
+return `PAYMENT BY YANDARU OFC
+	
 *さ E-MONEY*
 1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 089674504647
+		• YOGI ANDARU
 2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
+	ㅁ 089674504647
+		• YOGI ANDARU
 3. DANA
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 089674504647
+		• YOGI ANDARU
 
 *さ PULSA*
 
